0afdea18b295a14b471cd7072e0c29ea73922130
siamcodes <siamcodes@gmail.com>
2021-11-30 15:33:19 +0700

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Home </Text>
+            </View>
+        )
+    }
+}
+
